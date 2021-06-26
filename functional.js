61deb9486c254847d38f3741a10819a478be5bc3
hachi8833 <hachi8833@gmail.com>
2021-06-26 18:04:44 +0900

    Exercises

--- functional.js
+++ functional.js
@@ -1,10 +1,16 @@
 let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
 
+// URLに適した文字列を返す
+// 変換例: "North Dakota" -> "north-dakota"
+function urlify(string) {
+  return string.toLowerCase().split(/\s+/).join('-');
+}
+
 // URL生成: 命令型バージョン
 function imperativeUrls(elements) {
   let urls = [];
   elements.forEach(function(element) {
-    urls.push(element.toLowerCase().split(/\s+/).join("-"));
+    urls.push(urlify(element));
   });
   return urls;
 }
@@ -12,6 +18,11 @@ console.log(imperativeUrls(states));
 
 // URL生成: 関数型バージョン
 function functionalUrls(elements) {
-  return elements.map(element => element.toLowerCase().split(/\s+/).join('-'));
+  return elements.map(element => urlify(element));
 }
 console.log(functionalUrls(states));
+
+function validUrlList(list){
+  return list.map(elm => "https://example.com/" + urlify(elm));
+}
+console.log(validUrlList(states));

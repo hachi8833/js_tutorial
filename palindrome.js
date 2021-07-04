2f5c85c997aef00ffa5930f487f55fc87c1bf48f
hachi8833 <hachi8833@gmail.com>
2021-07-04 15:42:12 +0900

    Add TranslatedPhrase

--- palindrome.js
+++ palindrome.js
@@ -17,3 +17,19 @@ function Phrase(content) {
     return this.processedContent() === reverse(this.processedContent());
   }
 }
+
+// TranslatedPhraseオブジェクトを定義する
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // パリンドロームのテスト用に小文字に変換したcontentを返す
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // パリンドロームならtrueを、違うならfalseを返す
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+}

7aa6319d28c34eb0990c2b11ba0e0f6a2a31e6c1
hachi8833 <hachi8833@gmail.com>
2021-07-10 20:06:12 +0900

    Extend String.prototype

--- palindrome.js
+++ palindrome.js
@@ -1,36 +1,19 @@
-// 文字列を逆順にする
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// reverse()メソッドを追加してすべてのStringで使えるようにする
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
 // Phraseオブジェクトを定義する
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
-
-  // パリンドロームのテスト用に小文字に変換したcontentを返す
+  // パリンドロームのテスト用に変換した訳文を返す
   this.processedContent = function processedContent() {
     return this.content.toLowerCase();
   }
 
   // パリンドロームならtrueを、違うならfalseを返す
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-// TranslatedPhraseオブジェクトを定義する
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // パリンドロームのテスト用に処理した訳文を返す
-  this.processedContent = function processedContent() {
-    return this.translation.toLowerCase();
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();

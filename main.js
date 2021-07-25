9c3f936c4251636bcc3763fa66e62efbc581df96
hachi8833 <hachi8833@gmail.com>
2021-07-25 14:58:53 +0900

    Implement palindrome

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let Phrase = require("mhartl-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}"はパリンドロームです`);
+} else {
+  alert(`"${phrase.content}"はパリンドロームではありません`)
+}
